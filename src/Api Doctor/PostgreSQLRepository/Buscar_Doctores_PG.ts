c7758d46a57a9196c60fb5ba3f30f1da2a2a99c3
Gabriel Ojeda <92034051+gabo310599@users.noreply.github.com>
2022-06-12 00:23:28 -0400

    Especialidad JSON

--- src/Api Doctor/PostgreSQLRepository/Buscar_Doctores_PG.ts
+++ src/Api Doctor/PostgreSQLRepository/Buscar_Doctores_PG.ts
@@ -10,7 +10,7 @@ export class BuscarDoctoresPG implements DoctorRepository{
    try {
 
         const getRegistros = await pool.query(
-            "SELECT * FROM doctor"
+            "SELECT d.*, e.nombre as Especialidad FROM doctor d, doctor_especialidad de, especialidad e WHERE e.id=de.fk_especialidad and de.fk_doctor=d.id"
         );
 
         return getRegistros.rows;
