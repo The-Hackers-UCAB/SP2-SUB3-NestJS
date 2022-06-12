2f9a9f1d61a626454e3711fa4a05f5690a1a235c
abadillo <63127054+abadillo@users.noreply.github.com>
2022-06-11 22:02:09 -0400

    Add .env

--- src/Api Doctor/DB/ConectarBD.ts
+++ src/Api Doctor/DB/ConectarBD.ts
@@ -1,12 +1,15 @@
-
-const Pool = require ("pg").Pool;
+require('dotenv').config();
+const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user: "postgres", 
-    password: "171215", 
-    host: "localhost",
-    port: 5432, 
-    database: "ShortPaper2"
+  user: process.env.POSTGRES_USER,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
